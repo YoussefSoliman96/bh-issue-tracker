da1b424fc0d86bada6298764c0b6b1ae2426030b
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-22 06:48:03 +0200

    Handle form submission

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,7 +7,6 @@ import classnames from "classnames";
 
 const NavBar = () => {
   const currentPath = usePathname();
-  console.log(currentPath);
 
   const links = [
     { label: "Dashboard", href: "/" },
