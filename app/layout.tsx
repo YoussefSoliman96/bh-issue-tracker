d389bff612feff1339ec22abecf31850a738b9b1
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-22 06:24:54 +0200

    Suppress Hydration Warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.variable}>
         <Theme appearance="light" accentColor="violet">
           <NavBar />
