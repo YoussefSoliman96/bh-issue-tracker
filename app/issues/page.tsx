b6524b123e9722ed02da9461b5911a9e09929864
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-22 11:51:03 +0200

    Add markdown preview

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -8,7 +8,7 @@ import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  // await delay(2000);
+  await delay(500);
   return (
     <div>
       <IssueActions />
