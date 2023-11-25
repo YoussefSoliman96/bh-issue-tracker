22b1bf31e3406f68536bd8d4272072362738b3cf
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-26 01:14:51 +0200

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -16,14 +16,21 @@ const IssuesPage = async ({ searchParams }: Props) => {
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
+
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
+    : undefined;
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
     : undefined;
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
   // await delay(500);
   return (
