f7e7d3869bee4cbfd7403cc2a3963d0c9f873468
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-26 00:18:18 +0200

    Filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,9 +2,22 @@ import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "@/app/components/index";
 import IssueActions from "./IssueActions";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
   // await delay(500);
   return (
     <div>
