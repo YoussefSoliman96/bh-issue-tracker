8bacc4363e2d81b988ed68e5c0c5f8ffdb8871ca
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-26 07:27:31 +0200

    Add metadata

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -4,6 +4,7 @@ import IssueActions from "./IssueActions";
 import Pagination from "@/app/components/Pagination";
 import IssueTable, { IssueQuery, columnNames } from "./IssueTable";
 import { Flex } from "@radix-ui/themes";
+import { Metadata } from "next";
 
 interface Props {
   searchParams: IssueQuery;
@@ -48,3 +49,8 @@ const IssuesPage = async ({ searchParams }: Props) => {
 export const dynamic = "force-dynamic";
 
 export default IssuesPage;
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Issue List",
+  description: "View all project issues",
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import prisma from "@/prisma/client";
+import { Flex, Grid } from "@radix-ui/themes";
+import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
-import Pagination from "./components/Pagination";
-import IssueChart from "./IssueChart";
-import { Flex, Grid } from "@radix-ui/themes";
+import { Metadata } from "next";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -24,3 +24,8 @@ export default async function Home() {
     </>
   );
 }
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Dashboard",
+  description: "View a summary of project issues",
+};
