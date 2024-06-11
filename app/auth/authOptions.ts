17eaa713b7a991c1fb82fe2a0d61e39e9a7509b6
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2024-06-11 23:54:20 +0300

    Remove jwt

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -11,9 +11,6 @@ const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  session: {
-    strategy: "jwt",
-  },
 };
 
 export default authOptions;
